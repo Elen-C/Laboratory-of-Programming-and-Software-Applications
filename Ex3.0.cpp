8d22f6178e2f3a68d6b25deca49715d6ae43b52e
Eleni <75179996+Elen-C@users.noreply.github.com>
2025-11-12 10:32:29 +0200

    Add files via upload

---
+++ Ex3.0.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+int i,j;
+
+for (j=1 ; j<=5 ; j++)
+{
+
+for (i=1 ; i<=5 ; i++)
+{
+	if (i<=j)
+	{
+	cout << "* " ;
+}
+	else 
+	{
+	cout << "  ";
+}
+
+}
+
+cout << endl;
+
+}
+
+
+
+}
